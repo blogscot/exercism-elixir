518b1370c8ac30c5bdc95cddc70d42d911b6bb49
blogscot <iain.diamond@gmail.com>
2016-07-24 21:58:20 +0100

    Encode passes

--- run-length-encoding/rle.exs
+++ run-length-encoding/rle.exs
@@ -6,24 +6,43 @@ defmodule RunLengthEncoder do
   It should also be able to reconstruct the data into its original form.
   "1H1O1R1S1E" => "HORSE"
   """
-  @spec encode(String.t) :: String.t
 
-  defp displayListOfTuple(my_list) do
-    Enum.reduce(my_list, "", fn {k, v}, acc -> acc <> Integer.to_string(v) <> k end)
+  def encodeKeywordList(my_list) do
+    Enum.reduce(my_list, "", fn {k, v}, acc -> acc <> Integer.to_string(v) <> Atom.to_string(k) end)
   end
 
+  def chunk([]), do: []
+
+  def chunk(chars = [char|_]) do
+    seq = Enum.take_while(chars, &(&1==char))
+    rest = Enum.drop(chars, Enum.count(seq))
+    [seq] ++ chunk(rest)
+  end
+
+  def encode_list(list) do
+    list
+    |> Enum.map(&(String.to_charlist &1))
+    |> Enum.map(&(List.to_atom &1))
+    |> Enum.reduce(Keyword.new, fn letter, kws -> Keyword.update(kws, letter, 1, &(&1+1)) end)
+    |> encodeKeywordList
+  end
+
+  @spec encode(String.t) :: String.t
   def encode(string) do
     string
     |> String.split("", trim: true)
-    |> Enum.reduce(Map.new, fn letter, dict -> Dict.update(dict, letter, 1, &(&1+1)) end)
-    |> Map.to_list
-    |> displayListOfTuple
+    |> chunk
+    |> Enum.map(fn x -> encode_list x end)
+    |> Enum.reduce("", fn x, acc -> acc <> x end)
   end
 
-  @spec decode(String.t) :: String.t
-  def decode(string) do
-
-  end
+  # @spec decode(String.t) :: String.t
+  # def decode(string) do
+  #
+  # end
 end
 
-IO.inspect RunLengthEncoder.encode("HORSE")
+
+# r = RunLengthEncoder.encode("HORSE")
+# r = RunLengthEncoder.encode("WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB")
+# IO.puts(inspect r)
