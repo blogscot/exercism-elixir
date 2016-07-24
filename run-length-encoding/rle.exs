15c2356dbd3f603f964001f44e76b060d40160b4
blogscot <iain.diamond@gmail.com>
2016-07-24 22:45:58 +0100

    Run Length Encoding

--- run-length-encoding/rle.exs
+++ run-length-encoding/rle.exs
@@ -1,24 +1,25 @@
 defmodule RunLengthEncoder do
-  @doc """
-  Generates a string where consecutive elements are represented as a data value and count.
-  "HORSE" => "1H1O1R1S1E"
-  For this example, assume all input are strings, that are all uppercase letters.
-  It should also be able to reconstruct the data into its original form.
-  "1H1O1R1S1E" => "HORSE"
-  """
 
   def encodeKeywordList(my_list) do
     Enum.reduce(my_list, "", fn {k, v}, acc -> acc <> Integer.to_string(v) <> Atom.to_string(k) end)
   end
 
-  def chunk([]), do: []
+  def group_by_char([]), do: []
 
-  def chunk(chars = [char|_]) do
+  @doc """
+  Separate a list of single letters into common groups.
+  e.g. ["A", "A", "A", "B", "C"] => [["A", "A", "A"], ["B"], ["C"]]
+  """
+  def group_by_char(chars = [char|_]) do
     seq = Enum.take_while(chars, &(&1==char))
     rest = Enum.drop(chars, Enum.count(seq))
-    [seq] ++ chunk(rest)
+    [seq] ++ group_by_char(rest)
   end
 
+  @doc """
+  Encode a list of single letter strings
+  ["A", "A", "A"] => "3A"
+  """
   def encode_list(list) do
     list
     |> Enum.map(&(String.to_charlist &1))
@@ -31,18 +32,30 @@ defmodule RunLengthEncoder do
   def encode(string) do
     string
     |> String.split("", trim: true)
-    |> chunk
+    |> group_by_char
     |> Enum.map(fn x -> encode_list x end)
     |> Enum.reduce("", fn x, acc -> acc <> x end)
   end
 
-  # @spec decode(String.t) :: String.t
-  # def decode(string) do
-  #
-  # end
+  def decode_sequence([]) do
+    ""
+  end
+
+  @doc """
+  Decode a list of encoded strings
+  ["3", "A", "1", "B"] => "AAAB"
+  """
+  def decode_sequence(list) do
+    [digit, letter] = Enum.take(list, 2)
+    rest = Enum.drop(list, 2)
+    String.duplicate(letter, String.to_integer digit) <> decode_sequence(rest)
+  end
+
+  @spec decode(String.t) :: String.t
+  def decode(string) do
+    Regex.scan(~r/\d+|\D+/, string)
+    |> List.flatten
+    |> decode_sequence
+  end
+
 end
-
-
-# r = RunLengthEncoder.encode("HORSE")
-# r = RunLengthEncoder.encode("WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB")
-# IO.puts(inspect r)
