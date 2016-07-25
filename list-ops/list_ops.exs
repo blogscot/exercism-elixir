4b11414a81f7b5fbc505b7d239db8b11d6dd597c
blogscot <iain.diamond@gmail.com>
2016-07-25 20:15:29 +0100

    List Ops

---
+++ list-ops/list_ops.exs
@@ -0,0 +1,49 @@
+defmodule ListOps do
+
+  @spec count(list) :: non_neg_integer
+  def count(l) do
+    reduce(l, 0, fn _, acc -> acc + 1 end)
+  end
+
+  @spec reverse(list) :: list
+  def reverse(l) do
+    reduce(l, [], fn x, acc -> [x] ++ acc end)
+  end
+
+  @spec map(list, (any -> any)) :: list
+  def map(l, f) do
+    case l do
+      [] -> []
+      [h|t] -> [f.(h)] ++ map(t, f)
+    end
+  end
+
+  @spec filter(list, (any -> as_boolean(term))) :: list
+  def filter(l, f) do
+    reduce(l, [], fn x, acc ->
+      if f.(x) do [x] ++ acc else acc end
+    end) |> reverse
+  end
+
+  @type acc :: any
+  @spec reduce(list, acc, ((any, acc) -> acc)) :: acc
+  def reduce(l, acc, f) do
+    case l do
+      [] -> acc
+      [h|t] -> reduce(t, f.(h, acc), f)
+    end
+  end
+
+  @spec append(list, list) :: list
+  def append(a, b) do
+    reduce(reverse(a), b, fn x, acc -> [x | acc] end)
+  end
+
+  @spec concat([[any]]) :: [any]
+  def concat(ll) do
+    case ll do
+      [] -> []
+      [hl|tl] -> append(hl, concat(tl))
+    end
+  end
+end
