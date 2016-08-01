77ea07c99ddba53fa2eb827ec25002dd39d5a993
blogscot <iain.diamond@gmail.com>
2016-08-01 18:52:51 +0100

    Refactor Nucleotide Count

--- nucleotide-count/dna.exs
+++ nucleotide-count/dna.exs
@@ -1,44 +1,20 @@
 defmodule DNA do
   @nucleotides [?A, ?C, ?G, ?T]
 
-  @doc """
-  Counts individual nucleotides in a DNA strand.
-
-  ## Examples
-
-  iex> DNA.count('AATAA', ?A)
-  4
-
-  iex> DNA.count('AATAA', ?T)
-  1
-  """
   @spec count([char], char) :: non_neg_integer
   def count(strand, nucleotide) do
-    if nucleotide in @nucleotides and is_valid?(strand), do:
-      strand |> Enum.filter(&(&1===nucleotide)) |> length,
-    else:
-      raise(ArgumentError, message: "Invalid Argument")
+    unless(nucleotide in @nucleotides and is_valid?(strand), do:
+      raise(ArgumentError, message: "Invalid Argument"))
+
+      strand |> Enum.filter(&(&1===nucleotide)) |> length
   end
 
-
-  @doc """
-  Returns a summary of counts by nucleotide.
-
-  ## Examples
-
-  iex> DNA.histogram('AATAA')
-  %{?A => 4, ?T => 1, ?C => 0, ?G => 0}
-  """
   @spec histogram([char]) :: map
   def histogram(strand) do
-    if is_valid?(strand), do:
-      %{?A => count(strand, ?A),
-        ?T => count(strand, ?T),
-        ?C => count(strand, ?C),
-        ?G => count(strand, ?G),
-      },
-    else:
-      raise(ArgumentError, message: "Invalid Argument")
+    unless(is_valid?(strand), do:
+      raise(ArgumentError, message: "Invalid Argument"))
+
+    Map.new(@nucleotides, fn x -> {x, count(strand, x)} end)
   end
 
   defp is_valid?(strand), do:
