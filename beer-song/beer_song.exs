39b1357f6525751916ca20b6cce32dcc9ef0c6fb
blogscot <iain.diamond@gmail.com>
2016-08-02 17:26:48 +0100

    Refactor Beer Song

--- beer-song/beer_song.exs
+++ beer-song/beer_song.exs
@@ -3,17 +3,10 @@ defmodule BeerSong do
   Get a single verse of the beer song
   """
   @spec verse(integer) :: String.t
-  def verse(number) when number === 1 do
-    """
-No more bottles of beer on the wall, no more bottles of beer.
-Go to the store and buy some more, 99 bottles of beer on the wall.
-"""
-  end
-
   def verse(number) do
     """
-#{pluralise(number-1)} of beer on the wall, #{pluralise(number-1)} of beer.
-Take #{what?(number)} down and pass it around, #{pluralise(number-2)} of beer on the wall.
+#{pluralise(number-1, :capitalise)} of beer on the wall, #{pluralise(number-1)} of beer.
+#{action?(number)}, #{pluralise(number-2)} of beer on the wall.
 """
   end
 
@@ -22,26 +15,20 @@ Take #{what?(number)} down and pass it around, #{pluralise(number-2)} of beer on
   """
   @spec lyrics(Range.t) :: String.t
   def lyrics(range \\ 100..1) do
-    (for number <- range, do: verse(number) <> "\n")
-    |> Enum.join
-    |> String.replace_suffix("\n", "")
+    Enum.map(range, &(verse &1))
+    |> Enum.join("\n")
   end
 
-  defp pluralise(number, item \\ "bottle") do
-    cond do
-      number > 1 -> "#{number} #{item}s"
-      number === 1 -> "#{number} #{item}"
-      number === 0 -> "no more #{item}s"
-      true -> :error
-    end
-  end
+  defp pluralise(number), do: pluralise(number, :nothing)
+  
+  defp pluralise(1, _), do: "1 bottle"
+  defp pluralise(0, :capitalise), do: "No more bottles"
+  defp pluralise(0, _), do: "no more bottles"
+  defp pluralise(-1, _), do: "99 bottles"
+  defp pluralise(number, _), do: "#{number} bottles"
 
-  defp what?(number) do
-    cond do
-      number === 2 -> "it"
-      true -> "one"
-    end
-  end
+  defp action?(2), do: "Take it down and pass it around"
+  defp action?(1), do: "Go to the store and buy some more"
+  defp action?(_), do: "Take one down and pass it around"
 
 end
-
