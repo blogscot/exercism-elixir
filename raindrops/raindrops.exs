361b62aa1ce44438b7040c859d4939617a887775
blogscot <iain.diamond@gmail.com>
2016-08-06 21:39:37 +0100

    Refactor raindrops

--- raindrops/raindrops.exs
+++ raindrops/raindrops.exs
@@ -1,25 +1,15 @@
 defmodule Raindrops do
-  @doc """
-  Returns a string based on raindrop factors.
-
-  - If the number contains 3 as a prime factor, output 'Pling'.
-  - If the number contains 5 as a prime factor, output 'Plang'.
-  - If the number contains 7 as a prime factor, output 'Plong'.
-  - If the number does not contain 3, 5, or 7 as a prime factor,
-    just pass the number's digits straight through.
-  """
-
   @pairs [{3,"Pling"},{5,"Plang"},{7,"Plong"}]
 
   @spec convert(pos_integer) :: String.t
   def convert(number) do
-    result = for {k, v} <- @pairs do
-      if rem(number, k) === 0, do: v, else: ""
+    for {factor, msg} <- @pairs do
+      if rem(number, factor) === 0, do: msg, else: ""
     end
     |> Enum.join
-    case result do
-      "" -> Integer.to_string(number)
-      _ -> result
-    end
+    |> parse(number)
   end
+
+  defp parse(value, number) when value === "", do: Integer.to_string(number)
+  defp parse(value, _number), do: value
 end
