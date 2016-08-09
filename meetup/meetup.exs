943468dd78191517d6b7f4e4acfce2790d4e660a
blogscot <iain.diamond@gmail.com>
2016-08-09 20:24:52 +0100

    Refactor meetup

--- meetup/meetup.exs
+++ meetup/meetup.exs
@@ -9,6 +9,16 @@ defmodule Meetup do
 
   @type schedule :: :first | :second | :third | :fourth | :last | :teenth
 
+  @weekdays %{
+    monday: 1,
+    tuesday: 2,
+    wednesday: 3,
+    thursday: 4,
+    friday: 5,
+    saturday: 6,
+    sunday: 7
+  }
+
   @doc """
   Calculate a meetup date.
 
@@ -17,28 +27,22 @@ defmodule Meetup do
   """
   @spec meetup(pos_integer, pos_integer, weekday, schedule) :: :calendar.date
   def meetup(year, month, weekday, schedule) do
-    weekdays = [monday: 1, tuesday: 2, wednesday: 3, thursday: 4, friday: 5, saturday: 6, sunday: 7]
-
     last_day = :calendar.last_day_of_the_month(year, month)
-    {_, day_index} = List.keyfind(weekdays, weekday, 0)
-    for day <- 1..last_day do
-      {day, :calendar.day_of_the_week({year, month, day})}
+    day_index = @weekdays[weekday]
+    day = for d <- 1..last_day do
+      {d, :calendar.day_of_the_week({year, month, d})}
     end
     |> Enum.filter_map(fn {_, m} -> m == day_index end, fn {l, _} -> l end)
-    |> find_date(schedule)
-    |> wrap(year, month)
-  end
-
-  defp wrap(day, year, month) do
+    |> find_day(schedule)
     {year, month, day}
   end
 
-  defp find_date(dates, schedule) do
+  defp find_day(dates, schedule) do
     case schedule do
       :first -> dates |> List.first
-      :second -> dates |> Enum.drop(1) |> List.first
-      :third -> dates |> Enum.drop(2) |> List.first
-      :fourth -> dates |> Enum.drop(3) |> List.first
+      :second -> dates |> Enum.at(1)
+      :third -> dates |> Enum.at(2)
+      :fourth -> dates |> Enum.at(3)
       :last -> dates |> List.last
       :teenth -> dates |> Enum.filter(&(&1 > 12 and &1 < 20)) |> List.first
     end
