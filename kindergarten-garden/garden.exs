3b8c7ed5569d986fd39d86dcdb3be49e1429dedf
blogscot <iain.diamond@gmail.com>
2016-08-11 11:34:47 +0100

    Kindergarten garden

---
+++ kindergarten-garden/garden.exs
@@ -0,0 +1,48 @@
+defmodule Garden do
+  @doc """
+    Accepts a string representing the arrangement of cups on a windowsill and a
+    list with names of students in the class. The student names list does not
+    have to be in alphabetical order.
+
+    It decodes that string into the various gardens for each student and returns
+    that information in a map.
+  """
+  defmodule Student do
+    defstruct alice: nil, bob: nil
+  end
+
+  @names [:alice, :bob, :charlie, :david, :eve, :fred, :ginny, :harriet,
+           :ileana, :joseph, :kincaid, :larry]
+
+  @flowers %{"C": :clover, "G": :grass, "R": :radishes, "V": :violets}
+
+  @spec info(String.t(), list) :: map
+  def info(info_string, student_names \\ @names) do
+    children = students(student_names)
+    [row1, row2] = String.split(info_string, "\n")
+
+    children
+    |> Enum.map(fn {name, range} ->
+       {name,
+        String.slice(row1, range) <> String.slice(row2, range)
+        |> String.codepoints
+        |> Enum.map(fn plant -> @flowers[String.to_existing_atom(plant)] end)
+        |> List.to_tuple
+        }
+    end)
+    |> Enum.into(%{})
+  end
+
+  @spec students(String.t()) :: map
+  def students(names \\ @names) do
+    names
+    |> Enum.sort
+    |> Enum.with_index
+    |> Enum.map(fn {name, index} -> {name, index*2..index*2+1} end)
+    |> Enum.into(%{})
+  end
+
+end
+
+# r=Garden.info("VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV")
+# IO.inspect(r)
