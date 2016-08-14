ad2c1fd7e1a0d193b569d0132817e9f688e1d294
blogscot <iain.diamond@gmail.com>
2016-08-14 12:58:00 +0100

    Parallel solution

--- parallel-letter-frequency/frequency.exs
+++ parallel-letter-frequency/frequency.exs
@@ -7,19 +7,50 @@ defmodule Frequency do
   The number of worker processes to use can be set with 'workers'.
   """
   @spec frequency([String.t], pos_integer) :: map
-  def frequency(texts, workers) do
+  def frequency([], _), do: %{}
+  def frequency(texts, _worker) do
+    frequency_spawn(texts)
+    num_texts = length(texts)
+    freq_collect(num_texts)
+  end
 
+
+  defp frequency_spawn(texts) do
+    sender = self()
     texts
-    |> Enum.join
-    |> String.downcase
-    |> remove_punctuation
-    |> String.graphemes
-    |> Enum.reduce(Map.new, fn letter, dict ->
-      Map.update(dict, letter, 1, &(&1+1)) end)
+    |> Enum.each(fn text ->
+      spawn(fn ->
+        letter_freqs = parse_string(text)
+        send(sender, {:freq, letter_freqs})
+      end)
+    end)
+  end
+
+  defp freq_collect(n) do
+    Enum.reduce(1..n, %{}, fn _map, acc->
+      receive do
+        {:freq, freqs} -> merge(acc, freqs)
+      end
+    end)
+  end
+
+  defp merge(map1=%{}, map2=%{}) do
+    Map.merge(map1, map2, fn _k, v1, v2 ->
+      v1 + v2
+    end)
   end
 
   defp remove_punctuation(string) do
     Regex.replace(~r/[\p{P}\s\d]+/, string, "")
+  end
+
+  defp parse_string(string) do
+    string
+    |> String.downcase
+    |> remove_punctuation
+    |> String.graphemes
+    |> Enum.reduce(Map.new, fn letter, acc ->
+      Map.update(acc, letter, 1, &(&1+1)) end)
   end
 
 end
