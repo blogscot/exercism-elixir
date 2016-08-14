98a5fe2ef08830ca402f0e8ae01eadd7b6b1a0bc
blogscot <iain.diamond@gmail.com>
2016-08-14 15:31:05 +0100

    Refactor Parallel Letter Frequency

--- parallel-letter-frequency/frequency.exs
+++ parallel-letter-frequency/frequency.exs
@@ -8,30 +8,23 @@ defmodule Frequency do
   """
   @spec frequency([String.t], pos_integer) :: map
   def frequency([], _), do: %{}
-  def frequency(texts, _worker) do
-    frequency_spawn(texts)
-    num_texts = length(texts)
-    freq_collect(num_texts)
+  def frequency(texts, workers) do
+    pool = Enum.map(1..workers, fn _ -> spawn(&process_text/0) end)
+
+    Enum.zip(texts, Stream.cycle(pool))
+    |> Enum.map(&start_worker/1)
+    |> Enum.map(&process_result/1)
+    |> Enum.reduce(&merge/2)
   end
 
-
-  defp frequency_spawn(texts) do
-    sender = self()
-    texts
-    |> Enum.each(fn text ->
-      spawn(fn ->
-        letter_freqs = parse_string(text)
-        send(sender, {:freq, letter_freqs})
-      end)
-    end)
+  defp start_worker({text, worker_pid}) do
+    send(worker_pid, {:text, text, self})
   end
 
-  defp freq_collect(n) do
-    Enum.reduce(1..n, %{}, fn _map, acc->
-      receive do
-        {:freq, freqs} -> merge(acc, freqs)
-      end
-    end)
+  defp process_result(_) do
+    receive do
+      {:freq, freqs} -> freqs
+    end
   end
 
   defp merge(map1=%{}, map2=%{}) do
@@ -40,11 +33,19 @@ defmodule Frequency do
     end)
   end
 
+  def process_text do
+    receive do
+      {:text, text, from} -> letter_freqs = parse_string(text)
+      send(from, {:freq, letter_freqs})
+      process_text
+    end
+  end
+
   defp remove_punctuation(string) do
     Regex.replace(~r/[\p{P}\s\d]+/, string, "")
   end
 
-  defp parse_string(string) do
+  def parse_string(string) do
     string
     |> String.downcase
     |> remove_punctuation
